8ae4ba18cac75e01eadf39f148cedf64255ffaa4
Ben Greenberg <hello@yalladevrel.com>
2024-06-05 08:53:19 -0400

    add tag model testing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,11 @@
 class Tag
   include ActiveModel::Model
-  attr_accessor :name, :type
+  attr_accessor :id, :name, :type
+
+  validates :name, presence: true
 
   def save
+    validate!
     bucket = Rails.application.config.couchbase_bucket
     self.id ||= SecureRandom.uuid
     bucket.default_collection.upsert(id, to_hash)
@@ -13,5 +16,23 @@ class Tag
       'name' => name,
       'type' => 'tag'
     }
+  end
+
+  def self.count
+    cluster = Rails.application.config.couchbase_cluster
+    query = "SELECT COUNT(*) AS count FROM `realworld-rails` WHERE `type` = 'tag'"
+    result = cluster.query(query)
+    result.rows.first['count']
+  end
+
+  def self.find(id)
+    cluster = Rails.application.config.couchbase_cluster
+    query = "SELECT META().id, * FROM `realworld-rails` WHERE META().id = $1 AND `type` = 'tag'"
+    result = cluster.query(query, [id])
+    Tag.new(result.rows.first) if result.rows.any?
+  end
+
+  def validate!
+    raise ActiveModel::ValidationError, self if invalid?
   end
 end
