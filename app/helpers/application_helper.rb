101f8e7305cbad6882550a766d800ed15974ad46
Ben Greenberg <hello@yalladevrel.com>
2024-06-07 10:47:15 -0400

    render markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def markdown(text)
+    if text.nil? || text.empty?
+      return ''
+    end
+
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer, extensions = {})
+    markdown.render(text).html_safe
+  end
 end
